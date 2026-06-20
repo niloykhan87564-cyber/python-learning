cd918c60d81e875ef550b14759d507eaf3b6c6a6
MD: Ridoan <niloykhan87564@gmail.com>
2026-06-20 01:20:09 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Hello GitHub")
+ask = int(input("Enter a number: "))
+
+if ask > 0:
+    print("Num is positive")
+elif ask < 0:
+    print("Num is negative")
+else:
+    print("Num is zero")
+
+mull = int(input("How many times do you want to multiply your num? "))
+print(f'\nMultiplication Table of {ask}:')
+
+for i in range(1, mull + 1):
+    print(f"{ask} x {i} = {ask * i}")
+
+    print(f"{ask}X{i} = {ask*i}")
